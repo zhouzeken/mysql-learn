0492de59b81def98b7df9abfff4e89a1e325f611
zhouzeken <>
2020-11-02 17:30:13 +0800

    更新

--- union-all.sql
+++ union-all.sql
@@ -1,9 +1,9 @@
  -- 使用union all 代替or 查询
  
- --案例
+ --案例，该查询语句，去除ORDER BY id DESC可以正常使用索引(uid也有索引),加上ORDER BY 后索引失效
  SELECT * FROM gl_z_test where id=133  or id=134 or uid=147 ORDER BY id desc LIMIT 10;
  
- --改为
+ --优化改为
  SELECT * FROM 
 (
 (select * FROM gl_z_test where id="133")
